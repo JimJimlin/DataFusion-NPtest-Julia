475306132e603da400cd8dcbe45a2c73fa84a0b0
jimjimlin <jim3232ya@hotmail.com>
2015-08-27 11:48:08 +0800

    finish binomial table

---
+++ fusion/Binomial_table.jl
@@ -0,0 +1,18 @@
+#=
+- Binomial_table
+- last change: 8/27/2015
+=#
+
+function binomial_table(Number, Probability)
+
+  for successive_values = 0 : Number
+
+    binomial_sum = binomial(Number,successive_values)*((Probability)^successive_values)*((1-Probability)^(Number-successive_values));
+    println(" ")
+    println(Number," ",successive_values," ",binomial_sum)
+
+  end
+
+end
+
+binomial_table(5, 0.6)
